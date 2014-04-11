c6718d09f5084f4719b5196bd34677b0e39914e7
Tim Burks <tim@radtastical.com>
2014-04-10 21:32:58 -0700

    reorganization

--- step2.sh
+++ step2.sh
@@ -9,7 +9,7 @@ sudo apt-get install unzip -y
 sudo adduser --system -disabled-login control 
 sudo addgroup control
 
-# replace /home/control with the AgentBox repository
+# replace /home/control 
 sudo rm -rf /home/control
 sudo cp -r AgentKit/services/control /home/control
 
@@ -17,13 +17,13 @@ cd /home/control
 sudo mkdir -p nginx/logs
 sudo mkdir -p var
 sudo mkdir -p workers
-
-cd /home/control/control
-sudo nush tools/setup.nu
-sudo /usr/sbin/nginx 
-cd ..
+sudo chown -R control /home/control
+sudo chgrp -R control /home/control
 
 sudo cp upstart/agentio-control.conf /etc/init
-sudo chown -R control /home/control
 sudo initctl start agentio-control
 
+cd /home/control/control
+#sudo nush tools/setup.nu
+#sudo /usr/sbin/nginx 
+cd ..
